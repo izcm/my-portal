9daa7b14513f53f54c3bab719671a8eb6d024fb4
qTeePie <isabel@chaparian.no>
2025-11-01 22:43:29 +0100

    simplifying flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
-import { Home } from "./pages/Home.tsx";
+import blockSvg from "@/assets/block.svg";
+// import { Home } from "./pages/Home.tsx";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col">
       {/* NAVBAR */}
       <div>
-        <nav className="h-16 px-6 flex items-center justify-between border-b border-neutral-800">
-          <span className="font-bold text-lg">BitBlocks</span>
+        <nav
+          className="
+        flex items-center justify-between 
+        h-16 px-6 
+        border-b border-neutral-800
+        "
+        >
+          <span className="font-bold text-lg">A2Z Blocks</span>
           <div className="flex gap-6 text-sm">
             <a href="#">About</a>
             <a href="#">Solutions</a>
@@ -17,12 +24,37 @@ export default function App() {
         <div className="h-[2px] w-full bg-blue-500 animate-pulse"></div>
       </div>
 
-      <section className="flex items-center h-dvh justify-center">
-        <Home />
-      </section>
+      <div className="flex flex-col grow gap-4">
+        <section
+          className="
+            flex flex-col lg:flex-row grow min-h-[720px] 
+            items-center gap-4 px-4 py-4"
+        >
+          <div className="lg:w-2/5 px-4 py-4">
+            <h1 className=""> A2Z Blocks</h1>
+            <p>From blueprint to bytecode.</p>
+          </div>
+          <div className="lg:w-3/5 text-center">
+            <img
+              src={blockSvg}
+              alt="block visual"
+              className="w-86 inline-block w-86"
+            />
+          </div>
+        </section>
+        <div className="h-[2px] w-full bg-blue-500 animate-pulse"></div>
+        <section>
+          <h2>Demos</h2>
+          <div className="h-80 bg-blue-500">01</div>
+        </section>
+      </div>
 
       {/* FOOTER */}
-      <footer className="h-14 flex items-center justify-center border-t border-neutral-800 text-xs text-neutral-500">
+      <footer
+        className="
+      flex items-center justify-center h-14 
+      border-t border-neutral-800 text-xs text-neutral-500"
+      >
         © 2025 BITBLCOKS. All rights reserved.
       </footer>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,28 @@
-import blockSvg from "@/assets/block.svg";
+// import blockSvg from "@/assets/block.svg";
 
 export const Home = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between grow gap-4 max-w-7xl">
-      {/* Left Text */}
+    <div className="flex flex-col md:flex-row grow gap-4">
+      <div className="basis-full bg-blue-500">01</div>
+      <div className="basis-full bg-blue-500">01</div>
+    </div>
+  );
+};
+
+/*return (
+    <div className="flex flex-col md:flex-row items-center justify-between grow gap-4">
       <div className="flex-3 text-start px-8">
-        <h1 className="text-4xl font-bold mb-4">BitBlocks</h1>
+        <h1 className="text-4xl font-bold mb-4 us">A2Z Blocks</h1>
         <p className="text-neutral-400 mb-6">
-          Built different, deployed better.
+          From blueprint to bytecode.
         </p>
         <button className="px-5 py-2 rounded-md text-sm hover:bg-blue-700 transition">
           Check Demos
         </button>
       </div>
 
-      {/* Right Visual (placeholder) */}
       <div className="flex flex-2 items-center justify-center">
         <img src={blockSvg} alt="block visual" className="w-64" />
       </div>
     </div>
-  );
-};
+  );*/
